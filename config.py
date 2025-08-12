04fe941faf63b2f93200b8b2cd1670fc18ff6b08
Adamact <113307211+Adamact@users.noreply.github.com>
2025-08-12 16:18:16 +0200

    Fixed config
    Created 2 new files, added street data and location data

--- config.py
+++ config.py
@@ -1,66 +1,385 @@
 streets = {
     "Brun" : {
-        "Västerlång Gatan" : {
-            "Pris" : 100, #Integer
-            "Huspris" : 10,
+        "Västerlånggatan" : {
+            "Pris" : 1200, #Integer
+            "Huspris" : 1000,
             "Hyra" : {
-                "Inga" : 5,
-                "Alla" : 10,
-                1 : 15,
-                2 : 20,
-                3 : 30,
-        }
-    },
+                "Inga" : 40,
+                "Alla" : 80,
+                1 : 200,
+                2 : 600,
+                3 : 1800,
+                4 : 3200,
+                5 : 5000,
+            },
+            "Intecknas" : 600
+        },
         "Hornsgatan" : {
-            "Pris" : 110,
-            "Huspris" : 10,
+            "Pris" : 1200,
+            "Huspris" : 1000,
             "Hyra" : {
-                "Inga" : 5,
-                "Alla" : 10,
-                1 : 15,
-                2 : 20,
-                3 : 30,
+                "Inga" : 80,
+                "Alla" : 610,
+                1 : 400,
+                2 : 1200,
+                3 : 3600,
+                4 : 6400,
+                5 : 9000
+                }
+            },
+            "Intecknas" : 600
+    },
+    "Ljus-Blå" : {
+        "Folkungagatan" : {
+            "Pris" : 2000, #Integer
+            "Huspris" : 1000,
+            "Hyra" : {
+                "Inga" : 120,
+                "Alla" : 240,
+                1 : 600,
+                2 : 1800,
+                3 : 5400,
+                4 : 8000,
+                5 : 11000
+                },
+            "Intecknas" : 1000
+            },
+        "Götgatan" : {
+            "Pris" : 2000, #Integer
+            "Huspris" : 1000,
+            "Hyra" : {
+                "Inga" : 120,
+                "Alla" : 240,
+                1 : 600,
+                2 : 1800,
+                3 : 5400,
+                4 : 8000,
+                5 : 11000
+                },
+            "Intecknas" : 1000
+            },
+        "Ringvägen" : {
+            "Pris" : 2400, #Integer
+            "Huspris" : 1000,
+            "Hyra" : {
+                "Inga" : 160,
+                "Alla" : 320,
+                1 : 800,
+                2 : 2000,
+                3 : 6000,
+                4 : 9000,
+                5 : 12000
+                },
+            "Intecknas" : 1200
+            }   
+    },
+    "Rosa" : {
+        "S:T Eriksgatan" : {
+            "Pris" : 2800,
+            "Huspris" : 2000,
+            "Hyra" : {
+                "Inga" : 200,
+                "Alla" : 400,
+                1 : 1000,
+                2 : 3000,
+                3 : 9000,
+                4 : 12500,
+                5 : 15000
+            },
+            "Intecknas" : 1400
+        },
+        "Odengatan" : {
+            "Pris" : 2800,
+            "Huspris" : 2000,
+            "Hyra" : {
+                "Inga" : 200,
+                "Alla" : 400,
+                1 : 1000,
+                2 : 3000,
+                3 : 9000,
+                4 : 12500,
+                5 : 15000
+            },
+            "Intecknas" : 1400
+        },
+        "Valhallavägen" : {
+            "Pris" : 3200,
+            "Huspris" : 2000,
+            "Hyra" : {
+                "Inga" : 240,
+                "Alla" : 480,
+                1 : 1200,
+                2 : 3600,
+                3 : 10000,
+                4 : 14000,
+                5 : 18000
+            },
+            "Intecknas" : 1600
+        },
+    },
+    "Orange" : {
+        "Sturegatan" : {
+            "Pris" : 3600,
+            "Huspris" : 2000,
+            "Hyra" : {
+                "Inga" : 280,
+                "Alla" : 560,
+                1 : 1400,
+                2 : 4000,
+                3 : 11000,
+                4 : 15000,
+                5 : 19000
+            },
+            "Intecknas" : 1800
+        },
+        "Karlavägen" : {
+            "Pris" : 3600,
+            "Huspris" : 2000,
+            "Hyra" : {
+                "Inga" : 280,
+                "Alla" : 560,
+                1 : 1400,
+                2 : 4000,
+                3 : 11000,
+                4 : 15000,
+                5 : 19000
+            },
+            "Intecknas" : 1800
+        },
+        "Narvavägen" : {
+            "Pris" : 4000,
+            "Huspris" : 2000,
+            "Hyra" : {
+                "Inga" : 320,
+                "Alla" : 640,
+                1 : 1600,
+                2 : 4400,
+                3 : 12000,
+                4 : 16000,
+                5 : 20000
+            },
+            "Intecknas" : 2000
+        },
+    },
+    "Röd" : {
+        "Strandvägen" : {
+            "Pris" : 4400,
+            "Huspris" : 3000,
+            "Hyra" : {
+                "Inga" : 360,
+                "Alla" : 720,
+                1 : 1800,
+                2 : 5000,
+                3 : 14000,
+                4 : 17500,
+                5 : 21000
+            },
+            "Intecknas" : 2200
+        },
+        "Kungsträdgårdsgatan" : {
+            "Pris" : 4400,
+            "Huspris" : 3000,
+            "Hyra" : {
+                "Inga" : 360,
+                "Alla" : 720,
+                1 : 1800,
+                2 : 5000,
+                3 : 14000,
+                4 : 17500,
+                5 : 21000
+            },
+            "Intecknas" : 2200
+        },
+        "Hamngatan" : {
+            "Pris" : 4800,
+            "Huspris" : 3000,
+            "Hyra" : {
+                "Inga" : 400,
+                "Alla" : 800,
+                1 : 2000,
+                2 : 6000,
+                3 : 15000,
+                4 : 18500,
+                5 : 22000
+            },
+            "Intecknas" : 2400
+        },
+    },
+    "Gul" : {
+        "Vasagatan" : {
+            "Pris" : 5200,
+            "Huspris" : 3000,
+            "Hyra" : {
+                "Inga" : 440,
+                "Alla" : 880,
+                1 : 2200,
+                2 : 6600,
+                3 : 16000,
+                4 : 19500,
+                5 : 23000
+            },
+            "Intecknas" : 2600
+        },
+        "Kungsgatan" : {
+            "Pris" : 5200,
+            "Huspris" : 3000,
+            "Hyra" : {
+                "Inga" : 440,
+                "Alla" : 880,
+                1 : 2200,
+                2 : 6600,
+                3 : 16000,
+                4 : 19500,
+                5 : 23000
+            },
+            "Intecknas" : 2600
+        },
+        "Stureplan" : {
+            "Pris" : 5600,
+            "Huspris" : 3000,
+            "Hyra" : {
+                "Inga" : 480,
+                "Alla" : 960,
+                1 : 2400,
+                2 : 7200,
+                3 : 17000,
+                4 : 20500,
+                5 : 24000
+            },
+            "Intecknas" : 2800
+        },
+    },
+    "Grön" : {
+        "Gustav Adolfs Torg" : {
+            "Pris" : 6000,
+            "Huspris" : 4000,
+            "Hyra" : {
+                "Inga" : 520,
+                "Alla" : 1040,
+                1 : 2600,
+                2 : 7800,
+                3 : 18000,
+                4 : 22000,
+                5 : 25500
+            },
+            "Intecknas" : 3000
+        },
+        "Drottninggatan" : {
+            "Pris" : 6000,
+            "Huspris" : 4000,
+            "Hyra" : {
+                "Inga" : 520,
+                "Alla" : 1040,
+                1 : 2600,
+                2 : 7800,
+                3 : 18000,
+                4 : 22000,
+                5 : 25500
+            },
+            "Intecknas" : 3000
+        },
+        "Diplomatstaden" : {
+            "Pris" : 6400,
+            "Huspris" : 4000,
+            "Hyra" : {
+                "Inga" : 560,
+                "Alla" : 1120,
+                1 : 3000,
+                2 : 9000,
+                3 : 20000,
+                4 : 24000,
+                5 : 28000
+            },
+            "Intecknas" : 3200
+        },
+    },
+    "Blå" : {
+        "Centrum" : {
+            "Pris" : 7000,
+            "Huspris" : 4000,
+            "Hyra" : {
+                "Inga" : 700,
+                "Alla" : 1400,
+                1 : 3500,
+                2 : 10000,
+                3 : 22000,
+                4 : 26000,
+                5 : 30000
+            },
+            "Intecknas" : 3500
+        },
+        "Norrmalmstorg" : {
+            "Pris" : 8000,
+            "Huspris" : 4000,
+            "Hyra" : {
+                "Inga" : 1000,
+                "Alla" : 2000,
+                1 : 4000,
+                2 : 12000,
+                3 : 28000,
+                4 : 34000,
+                5 : 40000
+            },
+            "Intecknas" : 4000
+        },
+    },
+    "Station" : {
+        "Södra Station" : {
+            "Pris" : 4000,
+            "Hyra" : {
+                1 : 500,
+                2 : 1000,
+                3 : 2000,
+                4 : 4000
+            },
+            "Intecknas" : 2000
+        },
+        "Östra Station" : {
+            "Pris" : 4000,
+            "Hyra" : {
+                1 : 500,
+                2 : 1000,
+                3 : 2000,
+                4 : 4000
+            },
+            "Intecknas" : 2000
+        },
+        "Central Station" : {
+            "Pris" : 4000,
+            "Hyra" : {
+                1 : 500,
+                2 : 1000,
+                3 : 2000,
+                4 : 4000
+            },
+            "Intecknas" : 2000
+        },
+        "Norra Station" : {
+            "Pris" : 4000,
+            "Hyra" : {
+                1 : 500,
+                2 : 1000,
+                3 : 2000,
+                4 : 4000
+            },
+            "Intecknas" : 2000
+        },
+    },
+    "Statligt" : {
+        "Elverket" : {
+            "Pris" : 3000,
+            "Hyra" : {
+                1 : lambda x : x * 80,
+                2 : lambda x : x * 200
+            },
+            "Intecknas" : 1500
+        },
+        "Vattenledningsverket" : {
+            "Pris" : 3000,
+            "Hyra" : {
+                1 : lambda x : x * 80,
+                2 : lambda x : x * 200
             }
         }
-    },
-    "Ljus-Blå" : { #Uppdatera innanför
-        "Västerlång Gatan" : {
-            "Pris" : 100, #Integer
-            "Huspris" : 10,
-            "Hyra" : {
-                "Inga" : 5,
-                "Alla" : 10,
-                1 : 15,
-                2 : 20,
-                3 : 30,
-        }
-    },
-        "Hornsgatan" : {
-            "Pris" : 110,
-            "Huspris" : 10,
-            "Hyra" : {
-                "Inga" : 5,
-                "Alla" : 10,
-                1 : 15,
-                2 : 20,
-                3 : 30,
-            }
-        }
-    },
-    # Osv....
-
-}
-settings = {
-    "quick_loan" : {
-        "min_loan" : 100,
-        "max_loan" : 1000,
-        "interest" : 0.3
-    },
-    "market" : {
-     "dividend" : 0.02   
-    },
-    "player" : {
-        "start_balance" : 1000 
     }
-
 }
